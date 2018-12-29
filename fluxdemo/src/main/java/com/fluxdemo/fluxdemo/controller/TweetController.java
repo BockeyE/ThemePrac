ffa607425ae95798f0d00c4d3cab93550920ea2e
b <gymnopediece@163.com>
2018-12-29 19:54:32 +0800

    fluxdemo

--- fluxdemo/src/main/java/com/fluxdemo/fluxdemo/controller/TweetController.java
+++ fluxdemo/src/main/java/com/fluxdemo/fluxdemo/controller/TweetController.java
@@ -3,7 +3,9 @@ package com.fluxdemo.fluxdemo.controller;
 import com.fluxdemo.fluxdemo.model.Tweet;
 import com.fluxdemo.fluxdemo.repository.TweetRepository;
 import com.sun.deploy.nativesandbox.comm.Response;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -37,5 +39,24 @@ public class TweetController {
                 .map(savedTweet -> ResponseEntity.ok(savedTweet))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
+
+    @PostMapping("/tweets/{id}")
+    public Mono<ResponseEntity<Tweet>> updateTweetById(@PathVariable(value = "id") String id
+            , @Valid @RequestBody Tweet tweet
+    ) {
+        return tweetRepository.findById(id)
+                .flatMap(existingTweet -> {
+                    Tweet t = (Tweet) existingTweet;
+                    t.setText(tweet.getText());
+                    return tweetRepository.save(t);
+                })
+                .map(updateTweet -> new ResponseEntity<>(
+                        updateTweet, HttpStatus.OK
+                ))
+                .defaultIfEmpty(
+                        new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                );
+    }
+
 
 }
