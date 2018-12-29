8d4491138c46d1e634fbdd5e3675fd5ea45e4d40
b <gymnopediece@163.com>
2018-12-29 17:13:01 +0800

    Initial commit

---
+++ fluxdemo/src/main/java/com/fluxdemo/fluxdemo/FluxdemoApplication.java
@@ -0,0 +1,14 @@
+package com.fluxdemo.fluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxdemoApplication.class, args);
+    }
+
+}
+
