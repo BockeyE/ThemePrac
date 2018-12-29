a01f12ce50129d61f59ecd2af0cce607e7cf000d
b <gymnopediece@163.com>
2018-12-29 18:45:33 +0800

    fluxdemo

--- fluxdemo/src/main/java/com/fluxdemo/fluxdemo/controller/TweetController.java
+++ fluxdemo/src/main/java/com/fluxdemo/fluxdemo/controller/TweetController.java
@@ -2,8 +2,14 @@ package com.fluxdemo.fluxdemo.controller;
 
 import com.fluxdemo.fluxdemo.model.Tweet;
 import com.fluxdemo.fluxdemo.repository.TweetRepository;
-import org.springframework.web.bind.annotation.RestController;
+import com.sun.deploy.nativesandbox.comm.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 /**
  * @author bockey
@@ -11,11 +17,23 @@ import reactor.core.publisher.Flux;
 @RestController
 public class TweetController {
 
+    @Autowired
     private TweetRepository tweetRepository;
 
+    @GetMapping("/tweets")
     public Flux<Tweet> getAllTweets() {
-        return null;
+        return tweetRepository.findAll();
     }
 
+    @PostMapping("/tweets")
+    public Mono<Tweet> createTweets(@Valid @RequestBody Tweet tweet) {
+        return tweetRepository.save(tweet);
+    }
+
+
+    @GetMapping("/tweets/{id}")
+    public Mono<ResponseEntity<Tweet>> getTweetById(@PathVariable(value = "id") String id) {
+        return null;
+    }
 
 }
