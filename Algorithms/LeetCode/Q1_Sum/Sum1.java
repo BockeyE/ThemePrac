dccb2c1d12d6289a48561440aba516f768f133d3
b <gymnopediece@163.com>
2019-06-14 11:12:19 +0800

    leet

--- Algorithms/LeetCode/Q1_Sum/Sum1.java
+++ Algorithms/LeetCode/Q1_Sum/Sum1.java
@@ -6,6 +6,20 @@ import java.util.HashMap;
 
 /**
  * @author bockey
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Sum1 {
     public static int[] twoSum(int[] nums, int target) {
@@ -44,4 +58,7 @@ public class Sum1 {
 //        System.out.println(put);
 //        System.out.println(put2);
     }
+    /**
+     * 先跑一遍数组，把结果放入hashmap中，然后再跑一遍数组，到hashmap中找target-i的值即可，注意筛选出3，3这样同数的
+     * 在加入hash时就会弹回值，在此处判断，如果3，3不满足，则在第二次遍历时直接跳过即可     */
 }
